f47b932bf687ff0bac754a365227ed77fc95f0c9
jrtwheeler <jwheele1@gmail.com>
2021-11-14 21:14:52 -0600

    Added tableform component

---
+++ src/components/table_field/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Form = () => {
+    return(
+        <form>
+            <label>Title</label>
+            <input type="text"/>
+            <label>Author</label>
+            <input type="text"/>
+            <label>ISBN</label>
+            <input type="text"/>
+        </form>
+    )
+}
+
+export default Form;
