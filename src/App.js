4d9ff1ed3ca76ead8a71e0b3a829ba680fef4f56
jrtwheeler <jwheele1@gmail.com>
2021-11-14 21:10:37 -0600

    Added states for book manager

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
 function App() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [isbn, setIsbn] = useState("");
+  const [currentBook, setCurrentBook] = useState(null);
+  const [books, setBooks] = useState([
+    {
+      bookTitle: title,
+      bookAuthor: author,
+      bookIsbn: isbn,
+      bookId: uuidv4()
+    }
+  ]);
+
   return (
     <div className="App">
       <h1>Welcome</h1>
