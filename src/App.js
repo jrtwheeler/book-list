390cb404537247dc961c738dd9a096b3fc7ec08b
jrtwheeler <jwheele1@gmail.com>
2021-11-14 21:22:09 -0600

    Added states to form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React, { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import './App.css';
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import Form from "./components/form";
+import "./App.css";
 
 function App() {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
   const [isbn, setIsbn] = useState("");
-  const [currentBook, setCurrentBook] = useState(null);
+  const [currentBookId, setCurrentBookId] = useState(null);
   const [books, setBooks] = useState([
     {
       bookTitle: title,
       bookAuthor: author,
       bookIsbn: isbn,
-      bookId: uuidv4()
-    }
+      bookId: uuidv4(),
+    },
   ]);
 
   return (
     <div className="App">
-      <h1>Welcome</h1>
+      <div className="container">
+        <Form
+          title={title}
+          setTitle={setTitle}
+          author={author}
+          setAuthor={setAuthor}
+          isbn={isbn}
+          setIsbn={setIsbn}
+          currentBookId={setCurrentBookId}
+        />
+      </div>
     </div>
   );
 }
