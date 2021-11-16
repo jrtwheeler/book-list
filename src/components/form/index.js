e792670e7b77d7942996f4d0b338a60dcecd3582
jrtwheeler <jwheele1@gmail.com>
2021-11-15 22:05:18 -0600

    Added more props to form

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,28 +1,28 @@
 import React from "react";
 
 const Form = (props) => {
-    const {
-        title,
-        setTitle,
-        author,
-        isbn,
-        setUsbn,
-        currentBookId
-    } = props;
-    
-    return (
-        <form>
-            <label>Title</label>
-            <input 
-            type="text"
-            autoFocus
-            required
-            value={title}
-            onChange={(e) => setAuthor(e.target.value)}></input>
-            <label>Author</label>
-            <input type="text"></input>
-            <label>ISBN#</label>
-            <input type="text"></input>
-        </form>
-    )
-}
+  const { title, setTitle, author, isbn, setUsbn, currentBookId } = props;
+
+  return (
+    <form>
+      <label>Title</label>
+      <input
+        type="text"
+        autoFocus
+        required
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <label>Author</label>
+      <input
+        type="text"
+        autoFocus
+        required
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+      />
+      <label>ISBN#</label>
+      <input type="text"></input>
+    </form>
+  );
+};
