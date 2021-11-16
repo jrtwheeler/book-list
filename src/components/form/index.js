4d2dc278c6b33b9b4eebc7077ebd1d1471916358
jrtwheeler <jwheele1@gmail.com>
2021-11-15 21:59:00 -0600

    Added form

---
+++ src/components/form/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Form = () => {
+    return (
+        <form>
+            <label>Title</label>
+            <input type="text"></input>
+            <label>Author</label>
+            <input type="text"></input>
+            <label>ISBN#</label>
+            <input type="text"></input>
+        </form>
+    )
+}
