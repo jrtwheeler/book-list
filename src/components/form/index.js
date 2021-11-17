af38a437c0150a4e7f877b1b333f24f613635a56
jrtwheeler <jwheele1@gmail.com>
2021-11-16 22:32:29 -0600

    Add button tp form

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Form = (props) => {
-  const { title, setTitle, author, isbn, setUsbn, currentBookId } = props;
+  const { title, setTitle, author, isbn, setIsbn, currentBookId } = props;
 
   return (
     <form>
@@ -16,13 +16,21 @@ const Form = (props) => {
       <label>Author</label>
       <input
         type="text"
-        autoFocus
         required
         value={author}
         onChange={(e) => setAuthor(e.target.value)}
       />
       <label>ISBN#</label>
-      <input type="text"></input>
+      <input
+        type="text"
+        requiredvalue={isbn}
+        onChange={(e) => setIsbn(e.target.value)}
+      />
+      <button tabIndex="0" type="submit">
+        {currentBookId !== null ? "Update" : "Add"}
+      </button>
     </form>
   );
 };
+
+export default Form;
